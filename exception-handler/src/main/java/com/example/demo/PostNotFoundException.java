2355087990edd2cebf87f8e2e820fbb0a29cbd8e
Yura Nosenko <yura@attendify.com>
2019-07-09 09:56:30 +0300

    Pruning unused

--- exception-handler/src/main/java/com/example/demo/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo;
-
-public class PostNotFoundException extends RuntimeException {
-    public PostNotFoundException(Long id) {
-        super("Post:" + id +" is not found.");
-    }
-}
